a4befc67ae8e9184df311db8f5a77233cdbf4b73
Thieejhay <tijani.tofunmi@gmail.com>
2022-08-10 20:57:47 +0100

    completed pages

--- src/context/ResultContextProvider.js
+++ src/context/ResultContextProvider.js
@@ -7,7 +7,7 @@ const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
 export const ResultContextProvider = ({children})=>{
 const [data, setData] = useState([]);
 const [loading, setLoading] = useState(false);
-const [searchTerm, setSearchTerm] = useState('Buhari');
+const [searchTerm, setSearchTerm] = useState('');
 
 const getResults = async (searchType)=>{
     setLoading(true);
@@ -24,7 +24,13 @@ const getResults = async (searchType)=>{
 
     const results = await response.json();
     console.log(results)
-    setData(results);
+    if(searchType.includes('/news')){
+        setData(results.entries);
+    }
+    else if(searchType.includes('/image')){
+        setData(results.image_results)
+    }
+    else setData(results.results)
     setLoading(false);
 }
 
